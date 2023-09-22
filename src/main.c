028bba7b8aa284f32e236cce196e8efcbb0c2d4d
Denys Asauliak <denoming@gmail.com>
2023-09-22 16:28:25 +0300

    make refactoring

--- src/main.c
+++ src/main.c
@@ -1,91 +1,105 @@
-
 #include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
 
-#include <stdlib.h>
-#include <stdint.h>
-
-#include <esp_log.h>
 #include <esp_err.h>
+#include <esp_log.h>
 
-#include <driver/i2s.h>
+#include <driver/i2s_std.h>
 
-#include "webrtc_vad.h"
+#include <webrtc_vad.h>
 
-#define I2S_DMA_BUFFER_COUNT (4U)     // The total amount of DMA buffers count
-#define I2S_DMA_BUFFER_SIZE  (1024U)  // The size of each DMA buffer in samples
-#define I2S_SAMPLE_COUNT     (16384U) // The total amount of samples per data transfer
-#define I2S_SAMPLE_RATE      (16000U) // The total amount of samples per second
+/* Pins configuration */
+#define I2S_INMP441_SCK (GPIO_NUM_26)
+#define I2S_INMP441_WS (GPIO_NUM_22)
+#define I2S_INMP441_SD (GPIO_NUM_21)
 
-#define I2S_INMP441_PORT (I2S_NUM_0)
-#define I2S_INMP441_SCK  (GPIO_NUM_26)
-#define I2S_INMP441_WS   (GPIO_NUM_22)
-#define I2S_INMP441_SD   (GPIO_NUM_21)
+/* Audio configuration */
+#define I2S_SAMPLE_RATE (16000U) // samples per second
+#define I2S_SAMPLE_BYTES (4U)    // the amount of bytes in one sample
 
-#define VAD_FRAME_LENGTH  (20) // ms
-#define VAD_FRAME_SIZE    (I2S_SAMPLE_RATE / 1000 * VAD_FRAME_LENGTH)
+/* VAD configuration (supported 10, 20 and 30 ms frames and the rates 8000, 16000 and 32000 Hz) */
+#define VAD_FRAME_LENGTH (20)                                      // ms
+#define VAD_FRAME_SIZE (I2S_SAMPLE_RATE / 1000 * VAD_FRAME_LENGTH) // samples per ms * frame length
 
 static const char* TAG = "ESP32 I2S Mic VAD";
-static const TickType_t xDelay = 3000 / portTICK_PERIOD_MS;
 
-void mic_init()
+static i2s_chan_handle_t s_rx_handle;
+
+static void
+mic_init()
 {
-    i2s_config_t i2s_config = {
-        .mode = I2S_MODE_MASTER | I2S_MODE_RX,
-        .sample_rate = I2S_SAMPLE_RATE,
-        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-        .communication_format = I2S_COMM_FORMAT_STAND_I2S,
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = I2S_DMA_BUFFER_COUNT,
-        .dma_buf_len = I2S_DMA_BUFFER_SIZE,
-        .use_apll = false,
-        .tx_desc_auto_clear = false,
-        .fixed_mclk = 0
-    };
+    /* Get the default channel configuration */
+    i2s_chan_config_t chan_cfg = I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
 
-    const i2s_pin_config_t i2s_pin_config = {
-        .bck_io_num = I2S_INMP441_SCK,
-        .ws_io_num = I2S_INMP441_WS,
-        .data_out_num = I2S_PIN_NO_CHANGE,
-        .data_in_num = I2S_INMP441_SD
-    };
+    /* Allocate a new RX channel and get the handle of this channel */
+    ESP_ERROR_CHECK(i2s_new_channel(&chan_cfg, NULL, &s_rx_handle));
 
-    ESP_ERROR_CHECK(i2s_driver_install(I2S_INMP441_PORT, &i2s_config, 0, NULL));
-    ESP_ERROR_CHECK(i2s_set_pin(I2S_INMP441_PORT, &i2s_pin_config));
-    ESP_ERROR_CHECK(i2s_set_clk(I2S_INMP441_PORT, I2S_SAMPLE_RATE, I2S_BITS_PER_SAMPLE_32BIT, I2S_CHANNEL_MONO));
+    /* Setting the configurations */
+    i2s_std_config_t std_cfg = {
+      .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(I2S_SAMPLE_RATE),
+      /* Each sample has 32bits and only one channel (Mono) */
+      .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT,
+                                                  I2S_SLOT_MODE_MONO),
+      .gpio_cfg = {
+              .mclk = I2S_GPIO_UNUSED,
+              .bclk = I2S_INMP441_SCK,
+              .ws = I2S_INMP441_WS,
+              .dout = I2S_GPIO_UNUSED,
+              .din = I2S_INMP441_SD,
+              .invert_flags =
+                  {
+                      .mclk_inv = false,
+                      .bclk_inv = false,
+                      .ws_inv = false,
+                  },
+          },
+  };
+
+    /* Initialize the channel */
+    ESP_ERROR_CHECK(i2s_channel_init_std_mode(s_rx_handle, &std_cfg));
+
+    /* Before reading data, start the RX channel first */
+    ESP_ERROR_CHECK(i2s_channel_enable(s_rx_handle));
 }
 
-size_t mic_read(int16_t *samples, int count)
+static size_t
+mic_read(int16_t* samples)
 {
-    static int32_t buffer[I2S_DMA_BUFFER_SIZE / sizeof(int32_t)];
-    
-    size_t sample_index = 0;
-    while (count > 0) {
-        size_t bytes_need = count * sizeof(int32_t);
-        if (bytes_need > sizeof(buffer)) {
-            bytes_need = sizeof(buffer);
+    static const size_t s_buffer_size = 512U;
+    static int32_t s_buffer[512U];
+
+    int32_t sample_index = 0;
+    int32_t need_to_read = I2S_SAMPLE_RATE / 2;
+    while (need_to_read > 0) {
+        size_t bytes_read = 0;
+        if (i2s_channel_read(s_rx_handle,
+                             (char*) s_buffer,
+                             s_buffer_size * I2S_SAMPLE_BYTES,
+                             &bytes_read,
+                             portMAX_DELAY)
+            != ESP_OK) {
+            ESP_LOGE(TAG, "Unable to read from audio channel");
+            return 0;
         }
 
-        size_t bytes_read = 0;
-        ESP_ERROR_CHECK(i2s_read(I2S_INMP441_PORT, buffer, bytes_need, &bytes_read, portMAX_DELAY));
-
-        size_t samples_read = bytes_read / sizeof(int32_t);
-        for (int i = 0; i < samples_read; ++i) {
-            samples[sample_index] = buffer[i] >> 12;
-            sample_index++;
-            count--;
+        const size_t samples_read = bytes_read / I2S_SAMPLE_BYTES;
+        for (size_t i = 0; i < samples_read && need_to_read > 0; ++i) {
+            /* Get the highest 16 bits (the smaller, the louder it will be) */
+            samples[sample_index++] = s_buffer[i] >> 12;
+            /* Decrement needed to read samples  */
+            need_to_read--;
         }
     }
     return sample_index;
 }
 
-void mic_loop()
+_Noreturn void
+mic_loop()
 {
+    static const TickType_t xDelay = 3000 / portTICK_PERIOD_MS;
+
     int16_t* samples = (int16_t*) malloc(sizeof(uint16_t) * I2S_SAMPLE_RATE);
-    if (!samples) {
-        ESP_LOGE(TAG, "Failed to allocate memory for samples");
-        return;
-    }
+    assert(samples != NULL);
 
     VadInst* handle = WebRtcVad_Create();
     if (handle == NULL) {
@@ -104,30 +118,26 @@ void mic_loop()
         esp_restart();
     }
 
-    while(true) {
-        int samples_read = mic_read(samples, I2S_SAMPLE_RATE);
+    while (true) {
+        size_t samples_read = mic_read(samples);
         if (samples_read < VAD_FRAME_SIZE) {
-            ESP_LOGE(TAG, "Too few data");
+            ESP_LOGE(TAG, "Too few data for one VAD frame");
             continue;
         }
 
         size_t offset = 0;
         while (offset + VAD_FRAME_SIZE <= samples_read) {
-            int rv = WebRtcVad_Process(handle, I2S_SAMPLE_RATE, samples + offset, 320);
-            if (rv == +1) {
+            int rv = WebRtcVad_Process(handle, I2S_SAMPLE_RATE, samples + offset, VAD_FRAME_SIZE);
+            if (rv == 1) {
                 ESP_LOGI(TAG, "Voice detected");
-                break;
-            }
-            if (rv == -1) {
-                ESP_LOGI(TAG, "Error detected");
-                break;
             }
             offset += VAD_FRAME_SIZE;
         }
     }
 }
 
-void app_main()
+void
+app_main()
 {
     ESP_LOGI(TAG, "ESP32 Mic VAD Example Start");
     mic_init();
